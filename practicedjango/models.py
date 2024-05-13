8a6593f91522355aa6a0ad2c4d4d9b3b894d13bf
김재혁 <rlawogur329@mail.ulsan.ac.kr>
2024-05-13 22:35:18 +0900

    [Feat]: RestApi 구현 형태 작성 중

--- practicedjango/models.py
+++ practicedjango/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+class DataPoint(models.Model):
+    data = models.CharField(max_length=255)
+    timestamp = models.DataTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.data} at {self.timestamp}"
